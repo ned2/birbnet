d869f0d21b5ecb770a82ba60c83849b9db8d8ea9
Ned Letcher <nletcher@gmail.com>
2023-02-01 23:00:15 +1100

    add timestamp to logger entries

--- src/birbnet/__init__.py
+++ src/birbnet/__init__.py
@@ -1,5 +1,10 @@
 import logging
 
 logger = logging.getLogger(__package__)
+formatter = logging.Formatter(
+    datefmt="%Y-%m-%d %H:%M:%S",
+    fmt="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(message)s",
+)
 console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
